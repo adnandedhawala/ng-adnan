1add3a1a35dcb13de327df7b7770ebf52f152d12
adnandedhawala <adnan.d@somaiya.edu>
2019-07-29 22:31:10 +0530

    setting up project and crearting components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SliderComponent } from './slider/slider.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FeatureComponent } from './feature/feature.component';
+import { TabsComponent } from './tabs/tabs.component';
+import { RecommendedItemsComponent } from './recommended-items/recommended-items.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SliderComponent,
+    SidebarComponent,
+    FeatureComponent,
+    TabsComponent,
+    RecommendedItemsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
