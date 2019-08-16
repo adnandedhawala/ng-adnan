98544fd3bbb4df633cfdb9cd017aad415e5602be
adnandedhawala <adnan.d@somaiya.edu>
2019-08-17 00:55:42 +0530

    Added Login Component that has register and login form ||Added validations to form||Created custom validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import{RouterModule,Routes} from '@angular/router';
 import{HttpClientModule}from '@angular/common/http';
+import {ReactiveFormsModule} from '@angular/forms';
+import{FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component'; 
 import { HeaderComponent } from './header/header.component';
@@ -42,7 +44,9 @@ const appRoutes:Routes=[
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes),
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,53 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import{MustMatch} from '../customValidators/confirmPassword';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  public registerForm: FormGroup;
+  public loginForm: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {
+    this.registerForm = formBuilder.group({
+      userName: new FormControl(),
+      userEmail: new FormControl(),
+      userMobile: new FormControl(),
+      userPassword: new FormControl(),
+      userConfirmPassword: new FormControl()
+
+    });
+    this.loginForm = formBuilder.group({
+      userEmail: new FormControl(),
+      userPassword: new FormControl()
+    });
+  }
 
   ngOnInit() {
+    this.registerForm= this.formBuilder.group({
+      userName:['',[Validators.required,Validators.maxLength(15),Validators.minLength(2)]],
+      userMobile:['',Validators.required],
+      userEmail:['',[Validators.required,Validators.email]],
+      userPassword:['',Validators.required],
+      userConfirmPassword:['',Validators.required],
+    },{
+      validator:MustMatch('userPassword','userConfirmPassword')
+    });
+
+    this.loginForm= this.formBuilder.group({
+      userEmail:['',[Validators.required,Validators.email,]],
+      userPassword:['',Validators.required]
+    });
+  }
+
+  RegisterData(formData) {
+    console.log(formData);
+  }
+  LoginData(formData) {
+    console.log(formData);
   }
 
 }
+// please edit custom validation for password
